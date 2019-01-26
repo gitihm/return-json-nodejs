8ffd9431ab081bc27d07a43ce710a41984f5212d
gitihm <ismael897@outlook.com>
2019-01-27 02:03:53 +0700

    can return JSON & get post

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var body = require('body-parser')
 var http = require('http')
 var app = express()
 var PORT = 8081
@@ -12,14 +13,21 @@ var allowCrossDomain = function(req, res, next) {
     next();
 }
 
-app.use(allowCrossDomain)
-
+app.use(allowCrossDomain,body.json())
 app.get('/set/:name',(req,res)=>{
     user.name = req.params.name
     console.log("request : name "+user.name+" req "+req.params.name ,req.method,req.url)
     res.end(JSON.stringify(user))
 })
+app.post('/',(req,res)=>{
+    console.log("request  ",req.body.id ,req.method,req.url)
+    console.log( user.name +"= "+req.body.name)
+    user.name = req.body.name
+    res.end(JSON.stringify(user))
+    
+})
 app.get('/',(req,res)=>{
+    console.log("request  ",req.params.name ,req.method,req.url)
     res.send(JSON.stringify(user))
 })
 
